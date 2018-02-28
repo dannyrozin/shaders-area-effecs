dcdedac88591c86b437ff134f1579ef6c6c91ffc
dannyrozin <danny.rozin@nyu.edu>
2018-02-28 15:10:08 -0500

    2018
    2018

---
+++ shader_blur_under_mouse/myFragShader.glsl
@@ -0,0 +1,41 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+uniform sampler2D texture;  // this is the color of all the pixels coming in
+uniform vec2 texOffset;     // the x and y position of this pixels
+
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+uniform float x;                // this is our argument coming from processing
+uniform float y;                 // this is our argument coming from processing
+
+
+
+
+void main() {
+
+int amount;
+
+amount =  int(sqrt((x-vertTexCoord.x)*(x-vertTexCoord.x)+(y-vertTexCoord.y)*(y-vertTexCoord.y))*30); // calculate distance from mouse to our pixel
+
+if (amount > 20)amount =20;         // 20 in enough
+
+
+float countPix= 0;
+vec4 sumColors= vec4(0,0,0,0);                  // make a new vector to hold our sum (R,G,B,A)
+	for (int x = -amount;x<= amount;x++){        // lets visit all pixels around our pixels from -amount to +amount
+		for (int y = -amount;y<= amount;y++){
+		vec2 thisPixPosition = vertTexCoord.st + vec2(texOffset.s*x, texOffset.s*y);   // calculate the position of a pixel
+		  vec4 thisPixColor = texture2D(texture, thisPixPosition);                      // get the color of a pixel
+			sumColors+=thisPixColor;                                        // add the color of the pixel to our sum
+            countPix++;                                                     // keep count of how any pixels we added
+		}
+	}
+
+
+  gl_FragColor = sumColors/countPix;    // divide the sum with the number of pixels to get the average
+
+
+}
---
+++ shader_blur_under_mouse/shader_blur_under_mouse.pde
@@ -0,0 +1,31 @@
+// The world pixel by pixel 2018
+// Daniel Rozin
+// use fragment shader to do a blur on live video
+
+
+import processing.video.*;
+
+
+Capture video;
+PShader myFragShader;
+
+void setup() {
+  size(640, 480, P2D);
+  frameRate(400);
+  myFragShader = loadShader("myFragShader.glsl");
+  video = new Capture(this,width, height);
+  video.start();
+}
+void captureEvent(Capture c) {
+  c.read();
+}
+
+void draw() {
+  image(video,0,0);
+  float x = map(mouseX, 0,width, 0,1);
+  float y = map(mouseY, 0,height, 0,1);
+  myFragShader.set("x",x);
+  myFragShader.set("y",y);
+  shader(myFragShader);
+  println (frameRate);
+}
